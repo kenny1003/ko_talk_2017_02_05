f7fb3b2c68a67c72f9de7d7284ef2188f3c974c1
nemoland <nemoland0506@gmail.com>
2017-02-05 08:18:38 +0000

    nemoland

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'ko_tech_talk#index'
   get 'ko_tech_talk/index'
   haha
+  kim byeongchan
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
