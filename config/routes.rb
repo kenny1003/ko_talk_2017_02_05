9e18c0a6eb869de14491c57b45c9bfdae6337bf6
hoon <kwang3353@gmail.com>
2017-02-05 17:24:05 +0900

    kimkwanghoon

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'ko_tech_talk/index'
   haha
   kim byeongchan
+  kim kwanghoon
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
