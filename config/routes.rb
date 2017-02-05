b2c5c500f11c6d4cc0fdc4e9984d267874ceef61
nemoland <nemoland0506@gmail.com>
2017-02-05 08:27:32 +0000

    delete trash code

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   root 'ko_tech_talk#index'
   get 'ko_tech_talk/index'
-  haha
-  kim byeongchan
-  kim kwanghoon
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
